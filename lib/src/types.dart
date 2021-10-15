94b67e0bf67acb9eae3a38a49dd7da29d9768bec
Paolo <paolo.luchini97@gmail.com>
2021-10-15 12:45:23 +0200

    Define return type of GetRecoilValue

--- lib/src/types.dart
+++ lib/src/types.dart
@@ -2,6 +2,6 @@ import 'package:flutter/foundation.dart';
 import '../flutter_recoil.dart';
 
 typedef RecoilState<T> = T Function(RecoilOptions<T> atomOptions);
-typedef GetRecoilValue<T, H> = Function(RecoilState<H> recoilState);
+typedef GetRecoilValue<T, H> = T Function(RecoilState<H> recoilState);
 typedef GetAtomValue<T> = void Function(ValueNotifier<T> atomValue);
 typedef AtomEffect<T> = void Function(T onItemSet, ValueNotifier<T> setItemData);
